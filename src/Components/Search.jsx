36712c35aa0296803b3782ed86a3869cac55ce5f
Oliver Lluberes <olimail47@gmail.com>
2019-03-24 02:16:16 -0400

    search component added

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Search() {
   return (
-    <div>
-      
+    <div className={"search-form-container d-flex justify-content-center flex-wrap align-items-center"}>
+      <form className={"search-form d-flex justify-content-center  align-items-center"} action="">
+        <input className={"form-control input-search"} type="text" placeholder="Search" aria-label="Search"/>
+        <button type="submit" className={"btn btn-primary"}>Search</button>
+      </form>
     </div>
   )
 }
--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Title() {
+// This component will be use for all level 1 title in the page (h1)
+export default function Title(props) {
   return (
     <div>
-      
+      <h1>{this.props.title}</h1>
     </div>
   )
 }
