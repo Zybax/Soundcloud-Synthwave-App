76c9a951ef85c8dc8c2ba02bd2bf856ae92ff703
Oliver LLuberes <olimail47@gmail.com>
2019-03-26 20:20:09 -0400

    api items added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,11 +3,11 @@ import NavBar from './NavBar'
 
 export default function Header() {
   return (
-      <header className={"header d-flex justify-content-center flex-wrap align-items-start"}>
+      <header className={"header d-flex justify-content-center flex-wrap align-items-start align-content-arround"}>
         <NavBar/>
-        <div className={"cta-container d-flex justify-content-center flex-wrap align-items-center"}>
-          <h1 className={"cta-title"}>Find Awesome Darksynth and Synthwave Tracks</h1>
-          <h4 className={"cta-sub-title"}>Search by Artist, Name or Subgenre</h4>
+        <div className={"cta-container d-flex justify-content-center flex-wrap align-items-center "}>
+          <h1 className={"cta-title"}>Listen to Awesome Tracks</h1>
+          <h4 className={"cta-sub-title"}>Filter by Artist, Name or Genre</h4>
         </div>
       </header>
   )
