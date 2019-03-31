009d41f48872c5caddc4b2d417420d810884aeb2
Oliver Lluberes <olimail47@gmail.com>
2019-03-31 18:42:56 -0400

    feat: pagination

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
   return (
     <nav className={"nav d-flex flex-wrap justify-content-between"}>
-        <img src="" alt="logo"/>
+        
     </nav>
 )
 }
---
+++ src/Components/PageNumbers.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export default function PageNumbers(props) {
+  return (
+    <div className={"pagination-container d-flex justify-content-center"}>
+      <ul className={"pagination-list d-flex justify-content-center"}>{
+      props.pageNumbers.map(number => {
+      return (
+        <li
+        className={"pagination-list-item" + (props.currentPage === number ? " pagination-list-current":"")}
+        key={number}
+        id={number}
+        onClick={props.handlePageClick}
+        >
+          {number}
+        </li>
+      );
+      })
+    }
+      </ul>
+    </div>
+  )
+}
+
+PageNumbers.propTypes = {
+    pageNumbers: PropTypes.array.isRequired,
+    handlePageClick: PropTypes.func.isRequired
+  };
