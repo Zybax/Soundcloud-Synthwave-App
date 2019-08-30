c2437c8b4c0bf0fbbdb6c8c1dc8c63f1465e0d21
Oliver Lluberes <olimail47@gmail.com>
2019-08-30 09:11:48 -0400

    Updates

---
+++ precache-manifest.af6fbff837d642c1ea6566a98f427b2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "48ec05a7e1a4a615004de4f816236bcc",
+    "url": "/static/media/headerbackground.48ec05a7.jpg"
+  },
+  {
+    "revision": "c2c82cf35522fb9b6456e141863832f9",
+    "url": "/static/media/Neon.c2c82cf3.woff2"
+  },
+  {
+    "revision": "84f22513d15694072b0c49d4ccb2fbcb",
+    "url": "/static/media/Neon.84f22513.woff"
+  },
+  {
+    "revision": "9db99f1b54e749ad6c19",
+    "url": "/static/js/runtime~main.c9f4f9d2.js"
+  },
+  {
+    "revision": "f313834d199aca2bf230",
+    "url": "/static/js/main.59b57732.chunk.js"
+  },
+  {
+    "revision": "fbaf47833fd2209eba92",
+    "url": "/static/js/3.6fb32812.chunk.js"
+  },
+  {
+    "revision": "e2dbfa34d7ea0124ad27",
+    "url": "/static/js/2.78fba7cd.chunk.js"
+  },
+  {
+    "revision": "f313834d199aca2bf230",
+    "url": "/static/css/main.75a26e26.chunk.css"
+  },
+  {
+    "revision": "9bf66edfeca1576ae6af3043967d2086",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Zybax/Retro-Dreams/precache-manifest.925dba75ae5b8d86e25710e01f26a470.js"
+  "/precache-manifest.af6fbff837d642c1ea6566a98f427b2e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Zybax/Retro-Dreams/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
