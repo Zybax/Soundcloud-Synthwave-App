0c33e87796ab5757aa39af0752017437e1f58e90
Oliver Lluberes <olimail47@gmail.com>
2019-08-30 08:52:40 -0400

    Updates

---
+++ precache-manifest.925dba75ae5b8d86e25710e01f26a470.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "48ec05a7e1a4a615004de4f816236bcc",
+    "url": "/Zybax/Retro-Dreams/static/media/headerbackground.48ec05a7.jpg"
+  },
+  {
+    "revision": "c2c82cf35522fb9b6456e141863832f9",
+    "url": "/Zybax/Retro-Dreams/static/media/Neon.c2c82cf3.woff2"
+  },
+  {
+    "revision": "84f22513d15694072b0c49d4ccb2fbcb",
+    "url": "/Zybax/Retro-Dreams/static/media/Neon.84f22513.woff"
+  },
+  {
+    "revision": "3f53993ec164278c84af",
+    "url": "/Zybax/Retro-Dreams/static/js/runtime~main.1f59f1dd.js"
+  },
+  {
+    "revision": "5fb05f2775c75eae3e12",
+    "url": "/Zybax/Retro-Dreams/static/js/main.74e71de1.chunk.js"
+  },
+  {
+    "revision": "fbaf47833fd2209eba92",
+    "url": "/Zybax/Retro-Dreams/static/js/3.6fb32812.chunk.js"
+  },
+  {
+    "revision": "e2dbfa34d7ea0124ad27",
+    "url": "/Zybax/Retro-Dreams/static/js/2.78fba7cd.chunk.js"
+  },
+  {
+    "revision": "5fb05f2775c75eae3e12",
+    "url": "/Zybax/Retro-Dreams/static/css/main.3c0dbff6.chunk.css"
+  },
+  {
+    "revision": "ff43d3db5b2678529072f28db7e95cb6",
+    "url": "/Zybax/Retro-Dreams/index.html"
+  }
+];
